4111a8b9874c65bd03474f3af2e07e3383927170
EddiE Allen <eddie.xd@gmail.com>
2024-10-09 08:38:26 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const router = require('./routes');
 
-
-app.use(router);   
 app.use(express.json());
+app.use(router);   
+
 
 app.listen(5000, () => {
   console.log('servidor rodando na porta 5000');
