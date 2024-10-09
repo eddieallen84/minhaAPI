8461d3f9d27d8d126c24a45649310c0c046f0a12
Eddie Pinto - SENAI CUIABA <eddie.pinto@senaimt.ind.br>
2024-10-09 11:59:59 -0400

    Route post

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../users');
+const fs = require('fs');
 
+//ROTAS GET
 router.get('/', (req, res) => {
   res.send("REQUISIÇÃO OK");
 });
@@ -10,6 +12,49 @@ router.get('/users', (req, res) => {
   res.send(users);
 });
 
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((user) => user.id == id);
+  if (!user) {
+    return res.status(404).send("Usuário não encontrado");
+  }
+
+  res.status(200).send(user);
+});
+
+//ROTAS POST
+
+router.post("/users", (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  if (!nome || !email || !senha) {
+    return res
+      .status(400)
+      .send("Todos os campos são obrigatórios: nome, email e senha.");
+  }
+
+  const newUser = {
+    id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
+    nome,
+    email,
+    senha,
+  };
+
+  users.push(newUser);
+
+  const filePath = "../users.js"; 
+  
+  fs.writeFile(
+    filePath,
+    `let users = ${JSON.stringify(users, null, 2)};\n\nmodule.exports = users;`,
+    (err) => {
+      if (err) {
+        return res.status(500).send("Erro ao salvar o usuário.");
+      }
+      res.status(201).send(`Usuário ${nome} adicionado com sucesso!`);
+    }
+  );
+});
 
 
 module.exports = router;
---
+++ users.js
@@ -0,0 +1,46 @@
+let users = [
+  {
+    "id": 1,
+    "nome": "Eddie Allen",
+    "email": "eddieallen@gmail.com",
+    "senha": "123456"
+  },
+  {
+    "id": 2,
+    "nome": "Paulo Ricardo",
+    "email": "PauloRicardo@gmail.com",
+    "senha": "rpm2024"
+  },
+  {
+    "id": 3,
+    "nome": "Josephine",
+    "email": "josephine@gmail.com",
+    "senha": "josephine123"
+  },
+  {
+    "id": 4,
+    "nome": "Maria",
+    "email": "maria@gmail.com",
+    "senha": "maria123"
+  },
+  {
+    "id": 5,
+    "nome": "Pedro",
+    "email": "pedro@gmail.com",
+    "senha": "pedropedropedro"
+  },
+  {
+    "id": 6,
+    "nome": "Ayton Senna",
+    "email": "ayrton@senna.com",
+    "senha": "vaibrasil"
+  },
+  {
+    "id": 7,
+    "nome": "Roberto Carlos",
+    "email": "robertocarlos@gmail.com",
+    "senha": "roberto123"
+  }
+];
+
+module.exports = users;
